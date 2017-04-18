47d904c44f51619deade5c198e63875ef56fd4b9
akhil451 <akhilprasannan451@gmail.com>
2017-04-19 00:16:16 +0530

    a liite more complex require() implementation .

---
+++ 1.modules_exports/complex require/greet/english.js
@@ -0,0 +1,8 @@
+// Module English : exports a function that greets in English
+
+var greet = function (){
+console.log("Hello!");
+
+}
+
+module.exports=greet;
---
+++ 1.modules_exports/complex require/greet/index.js
@@ -0,0 +1,8 @@
+var english =require('./english');
+var hindi=require('./hindi');
+
+module.exports={
+ english:english,
+ hindi:hindi
+  
+};
